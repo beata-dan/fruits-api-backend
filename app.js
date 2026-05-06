c9565754e1c04e63a9363c25d36314a1ae87d72b
“beata-dan” <beata.danilevskaya@gmail.com>
2026-05-06 12:44:17 +0100

    Created html file and installed cors

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.get("/", (req, res) => {
     res.send("Hello from the fruits api")
 })
 
+app.use(cors()) // To allow cross origin connections to happen, essentially making the app less secure, but allows to update data 
 app.use(express.json()) //
 app.use("/fruits", fruits) // .use allows to create a function that points all of the routes that 
 
